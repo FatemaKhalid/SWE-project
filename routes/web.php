47c74c6765575905aa5e2c03091a394edcc8496c
FatemaKhalid <FatemaKhalid96@yahoo.com>
2017-05-18 03:52:35 +0200

    update routes

--- routes/web.php
+++ routes/web.php
@@ -16,37 +16,33 @@ Route::get('/', function () {
 });*/
 Route::get('/', function () {
     return view('index');
-});
+})->name('index');
 Route::get('/yarab', function () {
     return view('yarab');
 })->name('yarab');
 
-/*Route::get('/articles', function () {
-    return view('articles');
-})->name('articles');*/
+Route::get('/Adminarticles', function () {
+    return view('Adminarticles');
+})->name('Adminarticles');
 
-Route::get('/articles/{count_id}',[
+// Route::get('/Admincontacts', function () {
+//     return view('Admincontacts');
+// })->name('Admincontacts');
+Route::get('/Admincontacts','AdminController@contacts')->name('contacts');
+Route::post('/Admincontacts','AdminController@Addcont');//->name('contact');
+
+
+Route::get('/articles/{count_id}/{type}',[
     'as' => 'articles',
     'uses' =>'ArticleController@articles'
 ]);
-
-/*Route::get('/africa', function () {
-    return view('africa');
-})->name('africa');*/
-
-//Route::get('africa','homeController@africa')->name('africa');
-
-//Route::get('/africa/{loc_id}', ['uses' =>'homeController@africa']);
 
 Route::get('/africa/{loc_id}',[
     'as' => 'africa',
     'uses' =>'homeController@africa'
 ]);
 
-Route::get('/mainart',[
-    'as' => 'mainart',
-    'uses' =>'homeController@mainart'
-]);
+
 
 
 Route::get('/get/{id}',"ContinentController@get")->name("getcontinent");
@@ -78,14 +74,44 @@ Route::get('/image', function () {
 
 
 
+// awl 7route ele bygeb l countries
+Route::get('/Admincountry','ArticleController@countriesTodelete')->name('countriesTodelete');
+Route::get('/Adminhome', function () {
+    return view('Adminhome');
+})->name('Adminhome');
+
+
+Route::post('/Adminarticles','ArticleController@showTodelete')->name('showTodelete');
 
 
 
 Route::get('/insertArticle',"ArticleController@insert_article")->name('insertArticle');
 
-Route::get('/singlearticle/{article_id}',"ArticleController@get_single_article")->name('singlearticle');
+Route::get('/singlearticle/{article_id}/{count_id}',"ArticleController@get_single_article")->name('singlearticle');
 
 Route::get('/search4', function () {
     return view('search');
 });
-Route::any('/search',"ProductController@search_products");
+Route::post('/search',"ProductController@search_products")->name('search');
+
+
+Route::get('/increaselikes/{article_id}/{new_likes}',"ArticleController@increaselikes")->name('increaselikes');
+
+Route::get('/decreaselikes/{article_id}/{new_likes}',"ArticleController@increaselikes")->name('increaselikes');
+
+Route::get('/advicesMain',"AdviceController@advices")->name("advicesMain");
+
+Route::get('/singleadvice/{article_id}',"AdviceController@get_single_article")->name('singleadvice');
+
+Route::get('/increaselikesadvice/{article_id}/{new_likes}',"AdviceController@increaselikes")->name('increaselikes');
+
+Route::get('/decreaselikesadvice/{article_id}/{new_likes}',"AdviceController@increaselikes")->name('increaselikes');
+
+
+Route::post('/searchArticles',"ArticleController@search_articles")->name('searchArticles');
+Route::post('/searchAdvices',"AdviceController@search_advices")->name('searchAdvices');
+
+
+Route::post('/add_contact',"AdminController@Addcont")->name('add_contact');
+Route::post('/delete_contact',"AdminController@Delcont")->name('delete_contact');
+Route::post('/delArts',"ArticleController@delArts")->name('delArts');
